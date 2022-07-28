801eecf4f8066e67ac6b83cc3bf71639bed99a1f
Mike Esposito <mikeesposito6852@gmail.com>
2022-07-28 12:52:46 -0700

    Add form and capability to add a book to the library

--- script.js
+++ script.js
@@ -11,20 +11,42 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
+  myLibrary = [];
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
 
-addBookToLibrary("Infinite Jest", "DFW", 1100, "read");
-addBookToLibrary("Gain", "Richard Powers", 500, "read");
-addBookToLibrary("Godel, Escher, Bach", "Richard Hofstatder", 1200, "not read yet");
+let displayLibrary = () => {
+  myLibrary.forEach((item) => {
+    const p = document.createElement("p");
+    const book = document.createTextNode(item.info());
+    p.appendChild(book);
+    const library = document.getElementById("library");
+    library.appendChild(p);
+  });
+};
 
-console.log(myLibrary[0].info());
+const button = document.querySelector('#button');
+button.onclick = () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  let readOrNot;
+  if (document.getElementById('read').checked) {
+    readOrNot = 'read';
+  } else {
+    readOrNot = 'not read yet';
+  }
+  addBookToLibrary(title, author, pages, readOrNot);
+  displayLibrary();
+}
 
-myLibrary.forEach((item) => {
-  const p = document.createElement("p");
-  const book = document.createTextNode(item.info());
-  p.appendChild(book);
-  const library = document.getElementById("library");
-  library.appendChild(p);
-});
+const targetDiv = document.getElementById("form");
+const buttonTwo = document.getElementById("toggle");
+buttonTwo.onclick = () => {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
+  }
+};
