0e720a99bf877b83219e44f2c01830a1aa694fff
Mike Esposito <mikeesposito6852@gmail.com>
2022-08-22 13:51:34 -0700

    Refactor to use class syntax

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function() {
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  info() {
     return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
   }
 }
